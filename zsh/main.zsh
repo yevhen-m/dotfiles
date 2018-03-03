17bd89c902a572230bb2c0739a905f0a453a49ae
yevhen-m <yvhn.yvhn@gmail.com>
2018-03-03 13:09:31 +0200

    Speedup zsh startup time

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -23,6 +23,10 @@ setopt NO_CORRECT                # Turn off autocorrect
 setopt NO_INC_APPEND_HISTORY     # Write to the history file only when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 
+setopt EXTENDED_GLOB        # Use extended globbing syntax.
+unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+                            # Use >! and >>! to bypass.
+
 # Z settings
 Z_SCRIPT="$HOME/.config/z/z.sh"
 [[ -s $Z_SCRIPT ]] && source $Z_SCRIPT
@@ -43,7 +47,7 @@ BASE16_SHELL="$HOME/.config/base16-shell/scripts/base16-eighties.sh"
 # virtualenv-wrapper
 export WORKON_HOME=~/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
-VIRTUALENVWRAPPER="/usr/local/bin/virtualenvwrapper.sh"
+VIRTUALENVWRAPPER="/usr/local/bin/virtualenvwrapper_lazy.sh"
 [[ -s $VIRTUALENVWRAPPER ]] && source $VIRTUALENVWRAPPER
 
 # Activate virtualenv in a child shell
@@ -62,6 +66,8 @@ export PATH="/usr/local/sbin:$PATH"
 
 # Try to fix disappering cursor in gnome-terminal
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+
+ZSH_AUTOSUGGEST_USE_ASYNC=1
 
 # Edit long commands with vim
 autoload edit-command-line
@@ -90,9 +96,12 @@ fi
 alias gd='git diff -w'
 alias gdc='git diff --cached -w'
 alias gp='git push origin HEAD'
+alias gu='git pull'
 alias gs='git status -sb'
-alias gco='git checkout'
+alias gc='git checkout'
+alias gcb='git checkout -b'
 alias glg='git lg'
+alias gl='git lg'
 
 alias rm=trash
 
@@ -107,31 +116,28 @@ alias ta="tmux attach"
 alias td="tmux detach"
 alias tstart='tmux start-server; tmux attach'
 
-alias dotfiles="ls -a | grep '^\.' | grep --invert-match '\.DS_Store\|\.$'"
-
-# Utils
-# ================================================
-# Easily create sessions or switch to existing sessions
-function _check_session() {
-    tmux ls | grep $1
-}
-function tmuxx() {
-    [[ $TMUX == "" ]] && tmux new-session -As $1 || tmux switch-client -t $1
-}
-# Pipe http output to less for better browsing
-function httpless {
-    http --pretty=all --print=hb "$@" | less -R;
-}
-
 # Use the same key to go foreground
 fgkey() { fg }
 zle -N fgkey
 bindkey '^Z' fgkey
 
-# Pyenv settings: should be at the end of the script
-# cause pyenv modified PATH
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# Copy-paste `pyenv init -` wihout rehash call because it's too slow
+export PATH="/Users/yevhen/.pyenv/shims:${PATH}"
+export PYENV_SHELL=zsh
+source '/usr/local/Cellar/pyenv/1.2.2/libexec/../completions/pyenv.zsh'
+pyenv() {
+  local command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")";;
+  *)
+    command pyenv "$command" "$@";;
+  esac
+}
 
 eval "$(direnv hook zsh)"

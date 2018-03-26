0b6f586214f391e8432c7186589d6e87fa109274
yevhen-m <yvhn.yvhn@gmail.com>
2018-03-26 11:16:38 +0300

    Add arc_review script

---
+++ scripts/arc_review.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+editor=/usr/local/bin/nvim
+
+function review {
+    output="$(arc lint --output compiler)"
+    if [ -z "$output" ];
+    then
+        echo No lint errors. Running \`arc diff\`...
+        arc diff "$@"
+        return
+    else
+        $editor -q <(echo $output) -c "cwindow | cc"
+        review "$@"
+    fi
+}
+
+review "$@"

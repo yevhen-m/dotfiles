0a602ff72e26f89db4379bda1f68d7c1846e890e
yevhen-m <yvhn.yvhn@gmail.com>
2018-06-29 18:16:34 +0300

    Fix arc_review script when not paths are lintable

--- scripts/arc_review.sh
+++ scripts/arc_review.sh
@@ -4,13 +4,13 @@ editor=/usr/local/bin/nvim
 
 function review {
     output="$(arc lint --output compiler)"
-    if [ -z "$output" ];
+    if [ -z "$output" ] || [ "$output" = "No paths are lintable." ];
     then
         echo No lint errors. Running \`arc diff\`...
         arc diff "$@"
         return
     else
-        $editor -q <(echo $output) -c "cwindow | cc"
+        $editor -q <(echo "$output") -c "cwindow | cc"
         review "$@"
     fi
 }

7fdd170eae158e6b143714a648e7c0fb19ae547e
yevhen-m <yvhn.yvhn@gmail.com>
2024-03-17 11:58:42 +0200

    Replace fzf git bindings with fzf-git.sh

--- bashrc
+++ bashrc
@@ -20,50 +20,15 @@ shopt -s interactive_comments
 shopt -s cdspell
 # Correct spelling errors during tab-completion
 shopt -s dirspell
+# Disable softward flow control
+stty -ixon
 
 HISTSIZE=1000000
 HISTFILESIZE=1000000
 HISTCONTROL=ignoreboth:erasedups
 HISTIGNORE='l':'workon':'j':'gl':'gs':'gd'
 
-if [[ -f ~/.fzf.bash ]]
-then
-    source ~/.fzf.bash
-    export FZF_DEFAULT_OPTS="--bind=ctrl-z:toggle-up --inline-info --height 50% --no-reverse --border=rounded --bind=alt-a:select-all+accept"
-    export FZF_DEFAULT_COMMAND='ag --hidden -g ""'
-    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    export FZF_CTRL_T_OPTS='--no-reverse'
-
-    _gen_fzf_default_opts() {
-
-    local color00='#2d2d2d'
-    local color01='#393939'
-    local color02='#515151'
-    local color03='#747369'
-    local color04='#a09f93'
-    local color05='#d3d0c8'
-    local color06='#e8e6df'
-    local color07='#f2f0ec'
-    local color08='#f2777a'
-    local color09='#f99157'
-    local color0A='#ffcc66'
-    local color0B='#99cc99'
-    local color0C='#66cccc'
-    local color0D='#6699cc'
-    local color0E='#cc99cc'
-    local color0F='#d27b53'
-
-    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
-" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
-" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
-
-    }
-
-    _gen_fzf_default_opts
-
-    source ~/.fzf-utils.bash
-fi
+source ~/Dotfiles/fzf-utils.bash
 
 if [[ -f `brew --prefix`/etc/profile.d/z.sh ]]
 then
--- fzf-utils.bash
+++ fzf-utils.bash
@@ -1,76 +1,56 @@
-# Git utils
-# ====================================================================================
+if [[ -f ~/.fzf.bash ]]
+then
+    source ~/.fzf.bash
+    export FZF_DEFAULT_OPTS="--bind=ctrl-z:toggle-up --inline-info --height 50% --no-reverse --border=rounded --bind=alt-a:select-all+accept"
+    export FZF_DEFAULT_COMMAND='ag --hidden -g ""'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+    export FZF_CTRL_T_OPTS='--no-reverse'
 
-is_in_git_repo() {
-  git rev-parse HEAD > /dev/null 2>&1
-}
+    _gen_fzf_default_opts() {
 
-fzf-down() {
-  fzf --height 50% "$@" --border=horizontal
-}
+    local color00='#2d2d2d'
+    local color01='#393939'
+    local color02='#515151'
+    local color03='#747369'
+    local color04='#a09f93'
+    local color05='#d3d0c8'
+    local color06='#e8e6df'
+    local color07='#f2f0ec'
+    local color08='#f2777a'
+    local color09='#f99157'
+    local color0A='#ffcc66'
+    local color0B='#99cc99'
+    local color0C='#66cccc'
+    local color0D='#6699cc'
+    local color0E='#cc99cc'
+    local color0F='#d27b53'
 
-select-changed() {
-  is_in_git_repo || return
-  git -c color.status=always status --short |
-  fzf-down -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
-  cut -c4- | sed 's/.* -> //'
-}
+    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
+" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
+" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
 
-select-branch() {
-  is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
-  fzf-down --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
-  sed 's/^..//' | cut -d' ' -f1 |
-  sed 's#^remotes/##'
-}
+    }
 
-select-tag() {
-  is_in_git_repo || return
-  git tag --sort -version:refname |
-  fzf-down --multi --preview-window right:70% \
-    --preview 'git show --color=always {} | head -'$LINES
-}
+    _gen_fzf_default_opts
 
-select-commit() {
-  is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-    --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
-  grep -o "[a-f0-9]\{7,\}"
-}
+    source ~/Dotfiles/fzf-git.sh
+fi
 
-# Checkout git commit
-checkout-commit() {
-  local commits commit
-  commits=$(git log --graph --color=always --format="%C(auto)%h%d %s %C(241)%C(bold)%cr %C(auto)%C(blue)%cn") &&
-    commit=$(echo "$commits" | fzf-down --ansi --no-sort --reverse --tiebreak=index) && echo $commit &&
-    git checkout $(echo "$commit" | grep -E -o "[a-f0-9]{7,}")
-}
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"# Integration with z
 
-# Checkout git branch
-checkout-branch() {
-    local branches branch
-    branches=$(git branch -a --color=always | grep -v HEAD | sort) &&
-        branch=$(echo "$branches" |
-    fzf-down --ansi --tac -d $(( 2 + $(wc -l <<< "$branches") )) +m +s) &&
-        git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {}'"
 
-bind '"\er": redraw-current-line'
-bind '"\C-g\C-j": "$(select-changed)\e\C-e\er"'
-bind '"\C-g\C-i": "$(select-branch)\e\C-e\er"'
-bind '"\C-g\C-t": "$(select-tag)\e\C-e\er"'
-bind '"\C-g\C-h": "$(select-commit)\e\C-e\er"'
-bind '"\C-g\C-k": "$(checkout-branch)\e\C-e\er"'
-bind '"\C-g\C-o": "$(checkout-commit)\e\C-e\er"'
-
-# Other utils
-# ====================================================================================
-
-# Integration with z
 unalias z 2> /dev/null
 z() {
   [ $# -select-tag 0 ] && _z "$*" && return

99e7e812bafd8701f482397ee5eb338115d1a4ff
yevhen-m <yvhn.yvhn@gmail.com>
2024-04-29 13:31:56 +0300

    Refine statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -23,7 +23,7 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = { { "filename", path = 1 } },
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
@@ -39,7 +39,7 @@ return {
 			tabline = {},
 			winbar = {},
 			inactive_winbar = {},
-			extensions = {},
+			extensions = { "neo-tree", "lazy" },
 		})
 	end,
 }

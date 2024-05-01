62574f497772f2bf18be0bd37516c22ab67c69a7
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-01 16:22:24 +0300

    Add vim-tmux-navigator plugin

---
+++ nvim/lua/plugins/vim_tmux_navigator.lua
@@ -0,0 +1,24 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	},
+	init = function()
+		vim.cmd([[
+			let g:tmux_navigator_save_on_switch = 1
+			let g:tmux_navigator_disable_when_zoomed = 1
+			let g:tmux_navigator_preserve_zoom = 1
+		]])
+	end,
+}

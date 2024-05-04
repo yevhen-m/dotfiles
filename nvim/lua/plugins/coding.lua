4b186677137c35d1b764c5f0975444ddc52e615b
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-04 11:25:58 +0300

    Add vim-indent-object plugin for python

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,6 +1,7 @@
 return {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	"tpope/vim-rsi",
+	{ "michaeljsmith/vim-indent-object", ft = { "python", "lua" } },
 	{ "numToStr/Comment.nvim", opts = {} },
 	{
 		"windwp/nvim-autopairs",

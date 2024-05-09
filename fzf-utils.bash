116c3b501fb633546b732ee40dc4470ae59dfbcd
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-09 13:26:21 +0300

    Switch fzf colors to catppuccin

--- fzf-utils.bash
+++ fzf-utils.bash
@@ -5,7 +5,10 @@ then
     export FZF_DEFAULT_COMMAND='fd --type f'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_T_OPTS="--no-reverse --preview 'bat -n --color=always --line-range :500 {}'"
-    source ~/.local/share/nvim/lazy/tokyonight.nvim/extras/fzf/tokyonight_night.zsh
+    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
 fi
 
 # CTRL-Y to copy the command into clipboard using pbcopy

b1b284c34bca328fb4de8a19bd9924ec10702e67
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-18 20:08:38 +0300

    Switch tmux and vim to bubble status lines

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -6,7 +6,7 @@ return {
 				icons_enabled = true,
 				theme = "catppuccin-macchiato",
 				component_separators = { left = "|", right = "|" },
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
@@ -21,12 +21,14 @@ return {
 				},
 			},
 			sections = {
-				lualine_a = { "mode" },
+				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { { "filename", path = 1 } },
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_z = {
+					{ "location", separator = { right = "" }, left_padding = 2 },
+				},
 			},
 			inactive_sections = {
 				lualine_a = {},

90b333200f40f0061c7a44e0f3d4d6ceb4b3a695
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-22 08:01:34 +0300

    Add ipython config

---
+++ ipython_config.py
@@ -0,0 +1,14 @@
+from pygments.styles import get_style_by_name
+from pygments.util import ClassNotFound
+
+c = get_config()
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.separate_in = ""
+c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.true_color = True
+try:
+    c.TerminalInteractiveShell.highlighting_style = get_style_by_name(
+        "catppuccin-mocha"
+    )
+except ClassNotFound as ex:
+    print(f"Failed to set theme: {ex}")

3b64d9c55e0e5b7a1d52844cb34c30844ed4065b
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-25 10:16:12 +0300

    Turn on formatting for typescript

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -27,10 +27,8 @@ return {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
 			python = { "isort", "black" },
-			--
-			-- You can use a sub-list to tell conform to run *until* a formatter
-			-- is found.
 			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
 			json = { { "prettierd", "prettier" } },
 		},
 	},

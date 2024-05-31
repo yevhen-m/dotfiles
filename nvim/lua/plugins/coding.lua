1b0ee2960c9adce1353780e81f186b425bd5cfc6
yevhen-m <yvhn.yvhn@gmail.com>
2024-05-31 15:39:07 +0300

    Replace mini.surround with vim-surround

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -2,6 +2,7 @@ return {
 	"thinca/vim-visualstar",
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	"tpope/vim-rsi",
+	"tpope/vim-surround",
 	{ "numToStr/Comment.nvim", opts = {} },
 	{
 		"windwp/nvim-autopairs",
@@ -54,38 +55,6 @@ return {
 					vim.g.minipairs_disable = not vim.g.minipairs_disable
 				end,
 				desc = "Toggle Auto Pairs",
-			},
-		},
-	},
-	{
-		"echasnovski/mini.surround",
-		keys = function(_, keys)
-			-- Populate the keys based on the user's options
-			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-			local mappings = {
-				{ opts.mappings.add, desc = "Add Surrounding", mode = { "n", "v" } },
-				{ opts.mappings.delete, desc = "Delete Surrounding" },
-				{ opts.mappings.find, desc = "Find Right Surrounding" },
-				{ opts.mappings.find_left, desc = "Find Left Surrounding" },
-				{ opts.mappings.highlight, desc = "Highlight Surrounding" },
-				{ opts.mappings.replace, desc = "Replace Surrounding" },
-				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-			}
-			mappings = vim.tbl_filter(function(m)
-				return m[1] and #m[1] > 0
-			end, mappings)
-			return vim.list_extend(mappings, keys)
-		end,
-		opts = {
-			mappings = {
-				add = "gsa", -- Add surrounding in Normal and Visual modes
-				delete = "gsd", -- Delete surrounding
-				find = "gsf", -- Find surrounding (to the right)
-				find_left = "gsF", -- Find surrounding (to the left)
-				highlight = "gsh", -- Highlight surrounding
-				replace = "gsr", -- Replace surrounding
-				update_n_lines = "gsn", -- Update `n_lines`
 			},
 		},
 	},

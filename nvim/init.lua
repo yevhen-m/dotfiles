943d2a940237507d9ef3c3187f3535b451438268
yevhen-m <yvhn.yvhn@gmail.com>
2025-03-27 19:39:05 +0200

    Revert vim leader back to ,

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Hide mouse when typing
 vim.g.mousehide = true
